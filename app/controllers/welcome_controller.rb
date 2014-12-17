d227cb66f8da081b231b4788deeefd44edfa8de0
xinlei <wang.xinlei15@gmail.com>
2014-12-16 20:28:23 -0600

    created models

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,15 @@ class WelcomeController < ApplicationController
   def index
 
   end
+
+
+  def post
+
+  end
+
+  #Extract hashtags from the input text of the format #value1, #value2, ...
+  #Return a set of string
+  def parse
+
+  end
 end
